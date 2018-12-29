9ed17d46dbc6dd7cc688bf646dee8b71cdcc7c62
Yc.S <onixie@gmail.com>
2018-12-29 21:31:09 +0900

    Pass Day 11

--- src/Lv11/Ans.hs
+++ src/Lv11/Ans.hs
@@ -2,6 +2,8 @@ module Lv11.Ans (answerOfLv11Part1, answerOfLv11Part2) where
 
 import Data.Array (Array, array, (!), assocs)
 import Data.List (maximumBy)
+import Data.Tuple (swap)
+import Control.Concurrent (withMVar, newMVar, takeMVar, putMVar, forkFinally, newEmptyMVar, forkIO)
 
 type PowerLevel = Int
 
@@ -13,16 +15,38 @@ gridOfFuelCells serialNumber = array ((1, 1), (300, 300)) [ ((x, y), powerLevel 
                             digitInHundreds = startOfLv `div` 100 - startOfLv `div` 1000 * 10 in
       digitInHundreds - 5
 
-gridOfTotalPowerLevel :: Array (Int, Int) PowerLevel -> Array (Int, Int) PowerLevel
-gridOfTotalPowerLevel grid = array ((1, 1), (298, 298)) [ ((x,y)
-                                                  ,   grid ! (x, y)   + grid ! (x+1, y)   + grid ! (x+2, y)
-                                                    + grid ! (x, y+1) + grid ! (x+1, y+1) + grid ! (x+2, y+1)
-                                                    + grid ! (x, y+2) + grid ! (x+1, y+2) + grid ! (x+2, y+2)
-                                                  ) | x <- [1..298], y <- [1..298]]
+gridOfTotalPower :: Int -> Array (Int, Int) PowerLevel -> [((Int, Int), PowerLevel)]
+gridOfTotalPower n grid = let gridSize = 301 - n
+                              gridIndices = [ 1 .. gridSize ] 
+                              squareIndices = [ 0 .. n - 1 ] in
+                            [ ((x, y), sum [ grid ! (x + dx, y + dy) 
+                                            | dx <- squareIndices, dy <- squareIndices ]) 
+                            | x <- gridIndices, y <- gridIndices ]
 
-largestTotalPower = maximumBy ((. snd) . compare . snd) . assocs . gridOfTotalPowerLevel . gridOfFuelCells
+findLargestPower = maximumBy ((. snd) . compare . snd) 
 
-answerOfLv11Part1 = largestTotalPower 5719
+totalPower grid x y dx dy
+  | dy > 1 = calcRowPower grid x y dx + totalPower grid x (y+1) dx (dy-1)
+  | otherwise = calcRowPower grid x y dx
+  where 
+    calcRowPower grid x y dx
+      | dx > 1 = grid ! (x, y) + calcRowPower grid (x+1) y (dx-1)
+      | otherwise = grid ! (x, y)
 
+gridOfTotalPower' n grid = let range = [1..301 - n] in 
+  [((x,y), totalPower grid x y n n) | x <- range, y <- range]
 
-answerOfLv11Part2 = undefined
+largestTotalPower n = findLargestPower . gridOfTotalPower n
+
+answerOfLv11Part1 = largestTotalPower 3 $ gridOfFuelCells 5719
+
+onTotalPowers grid n m print
+  | n <= m = do
+      forkFinally (return (largestTotalPower n grid)) (\ltp -> print (n, ltp)) >> onTotalPowers grid (n+1) m print
+  | otherwise = return ()
+
+answerOfLv11Part2 :: IO ()
+answerOfLv11Part2 = do lock <- newMVar ()
+                       let atomicPrint str = withMVar lock (\_ -> print str)
+                       onTotalPowers (gridOfFuelCells 5719) 1 300 atomicPrint
+                       return ()
