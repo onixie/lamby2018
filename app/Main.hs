43703dc3497223c69fd51b8851dac5bf1b7d9550
Yc.S <onixie@gmail.com>
2018-12-30 16:24:03 +0900

    Revert "Try Seq Implementation"
    This reverts commit abea783e6c95e237d4f333a73bd3ede56802ed28.

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,4 @@ import Lv12.Ans
 main :: IO ()
 --main = answerOfLv9Part "/Users/shen/workdir/lamby2018/src/Lv9/sample.txt" 3 >>= print
 --main = answerOfLv10Part1 "/Users/shen/workdir/lamby2018/src/Lv10/input.txt"
-main = answerOfLv12Part "/Users/shen/workdir/lamby2018/src/Lv12/input.txt" 50000 >>= print >> return ()
+main = answerOfLv12Part2 "/Users/shen/workdir/lamby2018/src/Lv12/input.txt" 50000 >>= print >> return ()
--- src/Lv12/Ans.hs
+++ src/Lv12/Ans.hs
@@ -1,60 +1,59 @@
 {-# LANGUAGE DeriveFunctor #-}
-module Lv12.Ans (answerOfLv12Part1, answerOfLv12Part, answerOfLv12Part2) where
+module Lv12.Ans (answerOfLv12Part1, answerOfLv12Part2) where
 
 import Text.ParserCombinators.Parsec (ParseError, parse, sepEndBy, between, count, GenParser, string, many, many1, choice, char)
 import Text.Parsec.Char (spaces, newline)
 import Control.Arrow (right)
 import Data.List (find)
 import Data.Array (Array)
-import qualified Data.Sequence as S (Seq(..), fromList, take, drop, dropWhileL, dropWhileR, zip, (><), index, length, (<|), mapWithIndex, tails)
-import Data.Foldable (toList)
 
 type Pot = Char
 
-data Caven p = Caven { pots :: p, notes :: [(S.Seq Pot, Pot)] } deriving (Show, Functor)
+data Caven p = Caven { pots :: p, notes :: [([Pot], Pot)] } deriving (Show, Functor)
 
 plant = char '#'
 empty = char '.'
 pot = choice [plant, empty]
 
-initialState :: GenParser Char st (S.Seq Pot)
-initialState = string "initial state: " >> many1 pot >>= return . S.fromList
+initialState :: GenParser Char st [Pot]
+initialState = string "initial state: " >> many1 pot
 
-note :: GenParser Char st (S.Seq Pot, Pot)
+note :: GenParser Char st ([Pot], Pot)
 note = do 
-  llcrr <- count 5 pot >>= return . S.fromList
+  llcrr <- count 5 pot
   between spaces spaces $ string "=>"
   n <- pot
   return (llcrr, n)
 
-caven :: GenParser Char st (Caven (S.Seq Pot))
+caven :: GenParser Char st (Caven [Pot])
 caven = do
   is <- initialState
   many newline
   ns <- note `sepEndBy` newline
   return $ Caven is (filter (('#'==) . snd) ns)
 
-readCaven :: FilePath -> IO (Either ParseError (Caven (S.Seq Pot)))
+readCaven :: FilePath -> IO (Either ParseError (Caven [Pot]))
 readCaven filePath = readFile filePath >>= return . parse caven filePath
 
-generate :: Int -> Caven (S.Seq (Int, Pot)) -> Caven (S.Seq (Int, Pot))
 generate 0 caven = caven
 generate n caven@(Caven indexedPots notes) = generate (n-1) $ caven { pots = trimming $ nextGen padding notes }
   where
-    nextGen (_, S.Empty) _ = S.Empty
-    nextGen (startIndex, indexedPots) notes = (match startIndex notes (S.take 5 indexedPots)) S.<| nextGen (startIndex + 1, S.drop 1 indexedPots) notes
-    match startIndex notes pots = let pts = fmap snd pots in
-                                    case {-# SCC findNote #-} find ((pts ==) . fst) notes of
-                                      Just (_, pot) -> (startIndex, pot)
-                                      _ -> (startIndex, '.')
-    padding = let start = fst $ indexedPots `S.index` 0
-                  end   = fst $ indexedPots `S.index` (S.length indexedPots - 1) in 
-                (start - 2, S.fromList (zip [start - 4 .. start - 1] "....") S.>< indexedPots S.>< S.fromList (zip [end+1..end+4] "...."))
-    trimming = S.dropWhileR ((/= '#') . snd) . S.dropWhileL ((/= '#') . snd) 
+    nextGen []          notes = []
+    nextGen indexedPots notes = match (take 5 indexedPots) notes : nextGen (drop 1 indexedPots) notes
+    match pots notes = let ci = 2 + (fst $ head pots)
+                           pts = map snd pots in
+                         case {-# SCC findNote #-} find ((pts ==) . fst) notes of
+                           Just (pots, pot) -> (ci, pot)
+                           _ -> (ci, '.')
+    padding = let start = fst $ head indexedPots
+                  end   = fst $ last indexedPots in 
+                zip [start - 4 .. start - 1] "...." ++ indexedPots ++ zip [end+1..end+4] "...."
+    trimming = reverse . dropWhile ((/= '#') . snd) . reverse . dropWhile ((/= '#') . snd) 
 
-answerOfLv12Part f n = readCaven f
-                       >>= return . right (sum . map fst . filter ((=='#') . snd) . toList . pots . generate n . fmap (S.mapWithIndex (,)))
 
-answerOfLv12Part1 = answerOfLv12Part "input.txt" 20
+answerOfLv12Part1 = readCaven "input.txt" 
+  >>= return . right (sum . map fst . filter ((=='#') . snd) . pots . generate 20 . fmap (zip [0..]))
 
-answerOfLv12Part2 = answerOfLv12Part "input.txt" 5000
+answerOfLv12Part2 filePath g = readCaven filePath
+  >>= return . right (sum . map fst . filter ((=='#') . snd) . pots . generate g . fmap (zip [0..]))
+
