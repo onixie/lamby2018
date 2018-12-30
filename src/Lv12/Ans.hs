eec2a408e35231d9e3875321f196e9c3d03c4357
Yc.S <onixie@gmail.com>
2018-12-30 18:06:54 +0900

    Pass Day 12

--- src/Lv12/Ans.hs
+++ src/Lv12/Ans.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE DeriveFunctor #-}
-module Lv12.Ans (answerOfLv12Part1, answerOfLv12Part2) where
+
+module Lv12.Ans (answerOfLv12Part1, answerOfLv12Part2, answerOfLv12Part) where
 
 import Text.ParserCombinators.Parsec (ParseError, parse, sepEndBy, between, count, GenParser, string, many, many1, choice, char)
 import Text.Parsec.Char (spaces, newline)
 import Control.Arrow (right)
-import Data.List (find)
-import Data.Array (Array)
+import Data.Map.Strict (Map, fromList, findWithDefault)
+import Data.List (elemIndex)
 
 type Pot = Char
 
-data Caven p = Caven { pots :: p, notes :: [([Pot], Pot)] } deriving (Show, Functor)
+data Caven p = Caven { start :: Int, pots :: p, notes :: Map [Pot] Pot } deriving (Show, Functor)
 
 plant = char '#'
 empty = char '.'
@@ -30,30 +31,30 @@ caven = do
   is <- initialState
   many newline
   ns <- note `sepEndBy` newline
-  return $ Caven is (filter (('#'==) . snd) ns)
+  return $ Caven 0 is (fromList (filter (('#'==) . snd) ns))
 
 readCaven :: FilePath -> IO (Either ParseError (Caven [Pot]))
 readCaven filePath = readFile filePath >>= return . parse caven filePath
 
 generate 0 caven = caven
-generate n caven@(Caven indexedPots notes) = generate (n-1) $ caven { pots = trimming $ nextGen padding notes }
+generate n caven@(Caven start indexedPots notes) = let end = start + length indexedPots
+                                                       current = start - 2
+                                                       padding = "...." ++ indexedPots ++ "...." 
+                                                       nextGen = next current padding notes 
+                                                       Just nextStart = fmap (current+) $ elemIndex '#' nextGen in 
+                                                     generate (n-1) $ caven { start = nextStart, pots = trimming $ nextGen }
   where
-    nextGen []          notes = []
-    nextGen indexedPots notes = match (take 5 indexedPots) notes : nextGen (drop 1 indexedPots) notes
-    match pots notes = let ci = 2 + (fst $ head pots)
-                           pts = map snd pots in
-                         case {-# SCC findNote #-} find ((pts ==) . fst) notes of
-                           Just (pots, pot) -> (ci, pot)
-                           _ -> (ci, '.')
-    padding = let start = fst $ head indexedPots
-                  end   = fst $ last indexedPots in 
-                zip [start - 4 .. start - 1] "...." ++ indexedPots ++ zip [end+1..end+4] "...."
-    trimming = reverse . dropWhile ((/= '#') . snd) . reverse . dropWhile ((/= '#') . snd) 
+    next ci []          notes = []
+    next ci indexedPots notes = match ci (take 5 indexedPots) notes : next (ci + 1) (drop 1 indexedPots) notes
+    match ci pots notes = {-# SCC findNote #-} findWithDefault '.' pots notes
+    trimming = reverse . dropWhile ((/= '#')) . reverse . dropWhile ((/= '#'))
 
+answerOfLv12Part filePath n = do 
+  Right (Caven start pots _) <- readCaven filePath >>= return . right (generate n)
+  let ps = zip [start..] pots
+  return . sum . map fst $ filter ((=='#').snd) ps
 
-answerOfLv12Part1 = readCaven "input.txt" 
-  >>= return . right (sum . map fst . filter ((=='#') . snd) . pots . generate 20 . fmap (zip [0..]))
+answerOfLv12Part1 = answerOfLv12Part "input.txt" 20
 
-answerOfLv12Part2 filePath g = readCaven filePath
-  >>= return . right (sum . map fst . filter ((=='#') . snd) . pots . generate g . fmap (zip [0..]))
+answerOfLv12Part2 = answerOfLv12Part "input.txt" 500000
 
